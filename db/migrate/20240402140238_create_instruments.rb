6e763243f69221dad6b6543e3518f17055a4b018
Jakub-Ignatowicz <kuba_ign@proton.me>
2024-04-02 16:13:46 +0200

    Create migrations and migrate them

--- db/migrate/20240402140238_create_instruments.rb
+++ db/migrate/20240402140238_create_instruments.rb
@@ -1,8 +1,8 @@
 class CreateInstruments < ActiveRecord::Migration[7.1]
   def change
     create_table :instruments do |t|
-      t.string :ticker
-      t.string :name
+      t.string :ticker, limit: 20, null: false
+      t.string :name, limit: 256, null: false
 
       t.timestamps
     end
