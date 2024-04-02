702dedc0eca0d71e72850cc339eeb8a7263e777f
Jakub-Ignatowicz <kuba_ign@proton.me>
2024-04-02 15:01:31 +0200

    Implement default create handler

--- app/controllers/api/v1/exchanges_controller.rb
+++ app/controllers/api/v1/exchanges_controller.rb
@@ -1,2 +1,14 @@
-class ExchangesController < ApplicationController
+class Api::V1::ExchangesController < ApplicationController
+  include DefaultIndexHandler
+
+  private
+
+  def permitted_params
+    %i[mic name]
+  end
+
+  def model_params
+    params.require(:exchange).permit(*permitted_params)
+  end
 end
+
