a201fda11b4463463d160fec5303b9896bd615d0
Jakub-Ignatowicz <kuba_ign@proton.me>
2024-04-03 00:02:06 +0200

    Add optimistic locking to model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_02_144113) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_02_212246) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,6 +41,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_02_144113) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "instrument_id", null: false
+    t.integer "lock_version", default: 0
     t.index ["instrument_id"], name: "index_quotes_on_instrument_id"
   end
 
