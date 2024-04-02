7b1e9e48218d2ecf27627086b101389229c14a94
Jakub-Ignatowicz <kuba_ign@proton.me>
2024-04-02 14:25:41 +0200

    Add restrictions for exchange model

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -1,2 +1,11 @@
 class Exchange < ApplicationRecord
+  validates :mic, presence: true, length: { is: 4 },
+                  format: { with: /\A[a-zA-Z0-9]+\z/, message: 'mic can only contain letters and numbers' }
+  validates :name, presence: true, length: { maximum: 256 }
+
+  before_save :uppercase_mic
+
+  def uppercase_mic
+    self.mic = mic.upcase if mic
+  end
 end
