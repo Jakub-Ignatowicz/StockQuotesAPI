7733292cdc26e86551f81901a86bfe76bae7b8f7
Jakub-Ignatowicz <kuba_ign@proton.me>
2024-04-02 16:15:24 +0200

    Add default handlers to instruments controller

--- app/controllers/api/v1/instruments_controller.rb
+++ app/controllers/api/v1/instruments_controller.rb
@@ -1,2 +1,18 @@
-class InstrumentsController < ApplicationController
+class Api::V1::InstrumentsController < ApplicationController
+  include DefaultShowHandler
+  include DefaultIndexHandler
+  include DefaultDestroyHandler
+  include DefaultCreateHandler
+  include DefaultUpdateHandler
+
+  private
+
+  def permitted_params
+    %i[ticker name exchange_id]
+  end
+
+  def model_params
+    params.require(:instrument).permit(*permitted_params)
+  end
 end
+
