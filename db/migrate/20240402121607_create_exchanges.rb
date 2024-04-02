261f7a377d18f061ef5eee5c4af8f2c2661b69dc
Jakub-Ignatowicz <kuba_ign@proton.me>
2024-04-02 14:23:50 +0200

    Run a migration that creates exchanges in db

--- db/migrate/20240402121607_create_exchanges.rb
+++ db/migrate/20240402121607_create_exchanges.rb
@@ -1,10 +1,11 @@
 class CreateExchanges < ActiveRecord::Migration[7.1]
   def change
     create_table :exchanges do |t|
-      t.string :mic
-      t.string :name
+      t.string :name, limit: 256, null: false
+      t.string :mic, limit: 4, null: false
 
       t.timestamps
+      t.index ['mic'], name: 'index_exchanges_on_mic', unique: true
     end
   end
 end
